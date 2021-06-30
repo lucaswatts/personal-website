ba32661dca7fc858af80e81815b9957f3ccaaad1
lucaswatts <75272509+lucaswatts@users.noreply.github.com>
2021-06-30 00:22:35 -0600

    styled project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,10 +4,14 @@ import Layout from "../components/layout";
 const IndexPage = () => (
   <Layout>
     <div className='left'>
-      <h1>Projects.</h1>
+      <h1 id='projectsTitle'>Projects.</h1>
+      <div className='projectContainer'>
+      <iframe id='geoGuesser' src="https://friendly-ardinghelli-55203a.netlify.app/"></iframe>
+      <p className='projectDescription'>A react project that is a geographical quiz, where the user tries to match the displayed city to its respective country.</p>
+      <a className='projectLink' target='_blank' href='https://friendly-ardinghelli-55203a.netlify.app/'>Click here to see the project.</a>
+      </div>
     </div>
     <div className='right'>
-      <iframe src="https://friendly-ardinghelli-55203a.netlify.app/"></iframe>
     </div>
   </Layout>
 );
